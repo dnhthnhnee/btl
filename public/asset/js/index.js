b7e5f09d32299ccab1dd4d188316d46b31c813f9
HQL04 <hoquanglong04@gmail.com>
2024-04-06 23:29:21 +0700

    haha

--- public/asset/js/index.js
+++ public/asset/js/index.js
@@ -4,42 +4,47 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.10.0/firebas
 import { getDatabase, ref, set  } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDDSWubzk0D2gRjDXwv5E580WjTHvJwysc",
-    authDomain: "assignmenthospital-7d804.firebaseapp.com",
-    databaseURL: "https://assignmenthospital-7d804-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "assignmenthospital-7d804",
-    storageBucket: "assignmenthospital-7d804.appspot.com",
-    messagingSenderId: "573486781105",
-    appId: "1:573486781105:web:7d7cb8721bb8a1728a0890",
+  apiKey: "AIzaSyDDSWubzk0D2gRjDXwv5E580WjTHvJwysc",
+  authDomain: "assignmenthospital-7d804.firebaseapp.com",
+  databaseURL: "https://assignmenthospital-7d804-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "assignmenthospital-7d804",
+  storageBucket: "assignmenthospital-7d804.appspot.com",
+  messagingSenderId: "573486781105",
+  appId: "1:573486781105:web:7d7cb8721bb8a1728a0890"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth();
+
 const database = getDatabase(app);
-var dataRef = ref(database,'bacsi');
+console.log(database);
+var dataRef = ref(database,'1100');
 
 class MyObject {
   constructor() {
     this.data = {};
   }
-
   addProperty(key, value) {
     this.data[key] = value;
   }
-
   getData() {
     return this.data;
   }
 }
 const objectToWrite = new MyObject();
 
-$("#send").click(funtion(){
-    var email = $("#email").val();
-    var password = $("#password").val();
-    console.log(email);
-    console.log(password);
+$("#send").on("click", function(){
+    var email = $("#Email").val();
+    var password = $("#Password").val();
+    alert(email);
+    alert(password);
+   // console.log(email);
+    //console.log(password);
     SignIn(email,password);
+    alert("qua signin");
+    save(email,password);
+    alert("xong nha!");
 });
 
 function SignIn(email, password){
@@ -55,18 +60,17 @@ function SignIn(email, password){
   });
 }
 
-function save() {
-  let name      = document.getElementById('name').val();
-  let id        = document.getElementById('id').val();
-  let password  = document.getElementById('password').val();
-  dataRef = ref(database,'/'+id);
-  objectToWrite.addProperty(name, password);
-
-  set(dataRef, objectToWrite.getData())
+function save(email, password) {
+  const objectToWrite = {
+    key1: email,
+    key2: password,
+    // Thêm các thuộc tính khác nếu cần
+  };
+  set(dataRef, objectToWrite)
   .then(() => {
     console.log("Đã ghi đối tượng lên database thành công");
   })
   .catch((error) => {
     console.error("Lỗi khi ghi đối tượng lên database:", error);
   });
-};
+}
